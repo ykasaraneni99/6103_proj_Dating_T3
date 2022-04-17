8069d2805428efaa60d4f9ed9798be44a479e11b
ubhuiyan <90285310+ubhuiyan@users.noreply.github.com>
2022-04-17 14:53:16 -0400

    Update Question_Urnisha.py

--- Question_Urnisha.py
+++ Question_Urnisha.py
@@ -1,1 +1,6 @@
 # How likely an individual is to be seeing someone else based on sex, sexual orientation, and height?
+#%%
+import pandas as pd 
+dating = pd.read_csv("OkCupid_Data/okcupid_profiles.csv")
+
+# %%
