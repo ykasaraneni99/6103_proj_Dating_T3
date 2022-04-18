cf8122f8e3a43a9396acfa00795a93770573789f
kohishaa <97478052+kohishaa@users.noreply.github.com>
2022-04-17 22:54:20 -0400

    Modify

--- question.py
+++ question.py
@@ -7,6 +7,11 @@
 
 import numpy as np
 import pandas as pd
+import holoviews as hv
+import hvplot as hv
+import hvplot.pandas
+import holoviews as hv
+from hvplot import hvPlot
 
 dating = pd.read_csv("okcupid_profiles.csv")
 
@@ -22,6 +27,10 @@ print(new_data.head())
  
 
 #%%
+
+#
+dating.corr().style.background_gradient()
+
 # checking if there are any NaN values for one column
 new_data['drugs'].isnull().values.any()
 
@@ -33,9 +42,9 @@ new_dating_data = new_data.dropna()
 
 #%%
 replace_ty={#'body_type':{"a little extra":1,"average":2,"athletic":3,"skinny":4,"thin":5, "fit":6, "curvy":7, "full":9,"full figured":10, "jacked":11, "overweight":12, "used up":13, "rather not say":14},
-            'drinks':{'socially':1,"often":2,"not at all":3,"rarely":4, "very often":5},
+            'drinks':{'socially':1,"often":2,"not at all":3,"rarely":4, "very often":5, 'desperately' :6},
             'diet':{'strictly anything':1,'mostly other':2, 'mostly anything':3,'mostly vegetarian':4,'strictly vegan':5, 'anything':6, 'vegetarian':7, 'mostly halal':8, 'strictly vegetarian':9, 'other':10, 'strictly other': 11, 'vegan':12, 'mostly vegan':13, 'mostly kosher':14, 'strictly halal':15, 'halal':16, 'strictly kosher':17, 'kosher': 18},
-            'never':{'never': 1, 'sometimes': 2, 'often': 3},
+            'drugs':{'never': 1, 'sometimes': 2, 'often': 3},
             'smokes':{'sometimes':1, 'no':2, 'trying to quit':3, 'when drinking':4, 'yes':5}
            }
 
@@ -47,12 +56,14 @@ data =(new_dating_data.groupby("body_type")[["diet"]].count().sort_values(by="di
 data["% of participants"]=(data["diet"]/data["diet"].sum())*100
 data
 
+
 #%%
 ## body type for people who drink
 
-data_d =(new_dating_data.groupby("body_type")[["drinks"]].count().sort_values(by="drinks", ascending=False))
-data_d["% of participants"]=(data_d["drinks"]/data_d["drinks"].sum())*100
+data_d =(new_dating_data.groupby("drinks")[["body_type"]].count()) #.sort_values(by="body_type", ascending=False))
+data_d["% of participants"]=(data_d["body_type"]/data_d["body_type"].sum())*100
 data_d
+
 
 #%%
 ## body type for people who take drugs
@@ -71,15 +82,21 @@ import matplotlib as plt
 import seaborn as sns
 from matplotlib.pyplot import figure
 
-plt.figure(figsize=(10, 5))
+
+df_dating.plot('diet','body_type', kind='scatter', marker='o') 
+plt.ylabel('body type')
+plt.xlabel('diet')
+plt.show()
+
+#plt.figure(figsize=(10, 5))
 sns.countplot(x='diet', data=df_dating,
 hue='sex',
-order=df_dating['diet'].value_counts().iloc[:10].index)
-# %%
+order=df_dating['body_type'].value_counts().iloc[:10].index)
+#%%
 
 plt.figure(figsize=(15, 5))
 sns.countplot(x='drinks', data=df_dating,
-hue='sex',
+hue='body_type',
 order=df_dating['drinks'].value_counts().iloc[:10].index)
 
 # %%
